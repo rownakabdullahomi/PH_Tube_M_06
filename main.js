d2c62764914b6908f745e8a44fa7351a90e79d7a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-05 16:23:45 +0600

    dynamic data added done

--- main.js
+++ main.js
@@ -48,17 +48,31 @@ const displayVideos = (videos) => {
         const cardDiv = document.createElement("div");
         cardDiv.classList = "card bg-base-100 shadow-xl"
         cardDiv.innerHTML = `
-        <figure>
-    <img
-      src=${video.thumbnail} />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
+        <figure class="h-[250px] relative">
+            <img class="h-full w-full object-cover"
+            src=${video.thumbnail} />
+
+            ${video.others.posted_date? `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${video.others.posted_date}</span>` : ""}
+
+            
+        </figure>
+        <div class="px-0 py-2 flex gap-3 items-center">
+            <div>
+                <img class="h-12 w-12 rounded-full object-cover" src=${video.authors[0].profile_picture}/>
+            </div>
+            <div>
+                <h2 class="font-bold">${video.title}</h2>
+                <div class="flex items-center gap-2">
+                    <p class="text-gray-500">${video.authors[0].profile_name}</p>
+
+                    ${video.authors[0].verified? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`:""}
+
+                    
+                </div>
+                
+                <p></p>
+            </div>
+        </div>
         `;
 
     videosSection.append(cardDiv);
